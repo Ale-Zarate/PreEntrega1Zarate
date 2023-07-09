4d2a58768cbd1044869f2ecd213be5d182c1a9f2
Alejandro Zárate <115370538+Ale-Zarate@users.noreply.github.com>
2023-07-09 17:06:43 -0500

    Add files via upload

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import CartWidget from './CartWidget';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container">
+        <a className="navbar-brand" href="#">Mi Tienda</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Categoría 1</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Categoría 2</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Categoría 3</a>
+            </li>
+          </ul>
+          <CartWidget />
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
